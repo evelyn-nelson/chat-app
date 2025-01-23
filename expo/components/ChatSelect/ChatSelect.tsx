75b41f4c147577e1c93e38bdf4d46187f8f1c9bf
Evelyn Nelson <evelynnelson000@gmail.com>
2025-01-22 23:48:52 -0500

    Update ChatSelect UI platform-specific

--- expo/components/ChatSelect/ChatSelect.tsx
+++ expo/components/ChatSelect/ChatSelect.tsx
@@ -1,15 +1,40 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ScrollView,
+  RefreshControl,
+  Platform,
+} from "react-native";
 import { ChatSelectBox } from "./ChatSelectBox";
 import { ChatCreate } from "./ChatCreate/ChatCreate";
 import { useGlobalStore } from "../context/GlobalStoreContext";
 import { Group } from "@/types/types";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import ChatCreateModal from "./ChatCreate/ChatCreateModal";
+import { useWebSocket } from "../context/WebSocketContext";
 
 export const ChatSelect = () => {
-  const { store, groupsRefreshKey } = useGlobalStore();
+  const { store, groupsRefreshKey, refreshGroups } = useGlobalStore();
+  const { getGroups } = useWebSocket();
 
+  const [refreshing, setRefreshing] = useState<boolean>(false);
   const [groups, setGroups] = useState<Group[]>([]);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    try {
+      const data = await getGroups();
+      store.saveGroups(data);
+      refreshGroups();
+    } catch (error) {
+      console.error(error);
+    }
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 300);
+  }, []);
 
   useEffect(() => {
     store
@@ -19,29 +44,77 @@ export const ChatSelect = () => {
   }, [groupsRefreshKey]);
 
   return (
-    <View style={styles.container}>
-      <View>
-        <ChatCreateModal />
+    <View
+      style={
+        Platform.OS != "web"
+          ? [styles.container, styles.nativeWidth]
+          : [styles.container, styles.webWidth]
+      }
+    >
+      <ChatCreateModal />
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={[
+          styles.scrollContent,
+          Platform.select({
+            web: styles.scrollContentWeb,
+            native: styles.scrollContentMobile,
+          }),
+        ]}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            progressViewOffset={60}
+          />
+        }
+      >
+        <View style={styles.listPadding}></View>
         {groups.map((group, index) => {
+          const isLast = index === groups.length - 1;
           return (
             <ChatSelectBox
-              key={index}
+              key={group.id || index}
               group={{
                 ...group,
               }}
+              isLast={isLast}
             />
           );
         })}
-      </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  webWidth: {
+    width: 250,
+  },
+  nativeWidth: {
+    width: "100%",
+  },
   container: {
     flex: 1,
     marginTop: 50,
-    justifyContent: "flex-start",
+    borderRightWidth: 2,
+  },
+  scrollView: {
+    flex: 1,
+    width: "100%",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    alignItems: "center",
+  },
+  scrollContentWeb: {
     alignItems: "flex-start",
+  },
+  scrollContentMobile: {
+    alignItems: "flex-start",
+  },
+
+  listPadding: {
+    height: 10,
   },
 });
